757aec54298961b15d01fbd3588ba93a2bfaa8ef
Sam Scholefield <sam.scholefield@gmail.com>
2013-12-30 01:30:23 +0800

    numberHelpers init

---
+++ numberHelpers.js
@@ -0,0 +1,34 @@
+var minInArray = Math.min.apply(Math, array);
+
+var maxInArry = Math.max.apply(Math, array);
+
+function modeArray(array)
+{
+    if (array.length == 0)
+        return null;
+    var modeMap = {},
+        maxCount = 1, 
+        modes = [array[0]];
+
+    for(var i = 0; i < array.length; i++)
+    {
+        var el = array[i];
+
+        if (modeMap[el] == null)
+            modeMap[el] = 1;
+        else
+            modeMap[el]++;
+
+        if (modeMap[el] > maxCount)
+        {
+            modes = [el];
+            maxCount = modeMap[el];
+        }
+        else if (modeMap[el] == maxCount)
+        {
+            modes.push(el);
+            maxCount = modeMap[el];
+        }
+    }
+    return modes;
+}
