e07c7e0e5e594f94c1c04e1d02862963b25d40c3
Sam Scholefield <sam.scholefield@gmail.com>
2014-01-14 11:53:43 +0800

    key in object

--- objectHelpers.js
+++ objectHelpers.js
@@ -1,3 +1,5 @@
+
+//returns number of keys with non-null values
 Object.size = function(obj) {
     var size = 0, key;
     for (key in obj) {
@@ -5,3 +7,12 @@ Object.size = function(obj) {
     }
     return size;
   };
+
+  //returns an array of key names
+   var getKeys = function(obj){
+   var keys = [];
+   for(var key in obj){
+      keys.push(key);
+   }
+   return keys;
+}
